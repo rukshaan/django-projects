d65e8406da1345342d2e46630041103f639f7627
Rukshan <shaanruk0309@gmail.com>
2025-05-09 19:15:45 +0530

    contant .js file  and api.js errors checked

--- react_django_fullstack app_last/frontend/src/api.js
+++ react_django_fullstack app_last/frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {AccessToken} from './constantants'
+import {AccessToken} from './constants'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
